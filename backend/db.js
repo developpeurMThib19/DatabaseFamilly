706e2da341dc6b7b02ddeac1eb91a29381c47bd4
Thibault <developpeur.martin.thibault@gmail.com>
2025-07-25 15:08:07 +0200

    adapter la bdd pour  l heberger

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
+/*const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT),
-});
+});*/
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false, // obligatoire sur Render
+    }
+  });
 
 module.exports = pool;
