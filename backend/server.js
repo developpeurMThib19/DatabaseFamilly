e99b3541063145c252d5abf15d1be9744d833c42
Thibault <developpeur.martin.thibault@gmail.com>
2025-07-26 23:28:43 +0200

    fonctionner backend connexion

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,10 @@ app.use(cors({
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
+
+app.options('*', cors());
 
 // ✅ Middleware JSON
 app.use(express.json());
