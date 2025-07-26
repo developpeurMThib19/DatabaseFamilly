938db7b2f418fb2587f5fd0ebf4e56083aaf179e
Thibault <developpeur.martin.thibault@gmail.com>
2025-07-27 00:26:54 +0200

    test recup bakend

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
+  //credentials: true,
 }));
 
 app.options('*', cors());
