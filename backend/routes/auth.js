5eea8af80ee091b9cf8731992f5dc78eafda3c2a
Thibault <developpeur.martin.thibault@gmail.com>
2025-08-01 00:54:27 +0200

    rajout saltRounds

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 const pool = require('../db');
 const router = express.Router();
