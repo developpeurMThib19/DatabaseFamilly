342cd3a83fce368e1c98e82e8c6549759da6e6e9
Thibault <developpeur.martin.thibault@gmail.com>
2025-07-31 20:16:50 +0200

    ajout console.log cloudinary

--- backend/utils/cloudinaryStorage.js
+++ backend/utils/cloudinaryStorage.js
@@ -2,11 +2,14 @@ const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
 
+console.log("🔧 Cloudinary storage ready :", typeof storage._handleFile); // doit être "function"
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // bien corrigé ?
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+console.log("🔧 Cloudinary storage ready :", typeof storage._handleFile); // doit être "function"
 
 const storage = new CloudinaryStorage({
   cloudinary,
@@ -15,5 +18,6 @@ const storage = new CloudinaryStorage({
     allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
   },
 });
+console.log("🔧 Cloudinary storage ready :", typeof storage._handleFile); // doit être "function"
 
 const uploads = multer({ storage });
